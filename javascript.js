adaf522c00c531a2cc2c8d6f1c398823f55596f2
J. Cardenas <jonscardenas@gmail.com>
2017-11-20 14:54:24 -0600

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,35 +1,51 @@
 
 $(document).ready(function(){
 	var $mobileMenuOpen = false;
+  var $menuLock = false;
   
 	$(window).scroll(function(){
   	var $scroll = $(window).scrollTop();
     console.log($scroll);
     
     if ($scroll >= 220){
+    	$menuLock = true;
     	console.log("yoooo");
       $("#navigation").css("position", "fixed");
       $("#nav-list").css("top", "5px");
       $("#logo-small-desktop").css("display", "inline");
      	$("#logo-container").css("margin-bottom", "40px");
+      $("#mobile-nav-header").css("opacity", "1");
      
       /*$("#home-photo").css("top", "40px");
       $("#services-container").css("top", "37px");
       $("#about-container").css("top", "-3px");*/
     }else if ($scroll<220){
+    	$menuLock = false;
     	$("#navigation").css("position", "relative");
       $("#nav-list").css("top", "10px");
       $("#logo-small-desktop").css("display", "none");
       $("#logo-container").css("margin-bottom", "0px");
+      
+      if ($mobileMenuOpen == false){
+      	$("#mobile-nav-header").css("opacity", "0");
+      };
     }  
   });
   
   $("#mobile-nav-button").click(function(){
   	if ($mobileMenuOpen){
     	$('#mobile-menu-container').slideUp('slow');
+      $("#logo-link-container").css("visibility", "visible");
+      
+      if ($menuLock == false){
+      	$("#mobile-nav-header").css("opacity", "0");
+      };
+      
       $mobileMenuOpen = false;
     }else{
     	$('#mobile-menu-container').slideDown('slow');
+      $("#logo-link-container").css("visibility", "hidden");
+      $("#mobile-nav-header").css("opacity", "1");
       $mobileMenuOpen = true;
     }
   	
@@ -50,5 +66,13 @@ $(document).ready(function(){
     	$(this).css("color", "black");
   	}
 	);  
+  
+  $(".mobile-menu-item").hover(
+  	function(){
+    	$(this).css("backgroundColor", "#8BB2C0");
+    }, function(){
+      $(this).css("backgroundColor", "white");
+    }
+  );
   
 })
