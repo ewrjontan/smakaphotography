b0213ddbd95f5d5ec6e86f545081ecfff15befb2
J. Cardenas <jonscardenas@gmail.com>
2017-12-16 21:47:13 -0600

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,4 +1,74 @@
 
+// Contact form submission
+$(function() {
+  // Get the form.
+	var form = $('#ajax-contact');
+
+  // Get the messages div.
+  var formMessages = $('#form-messages');
+
+  // TODO: The rest of the code will go here...
+	console.log("form working");
+
+  // Event listener for the contact form.
+  $(form).submit(function(e) {
+      // Stop the browser from submitting the form.
+      console.log("Submit button has been pressed");
+      e.preventDefault();
+      
+      // Serialize the form data.
+			var formData = $(form).serialize();
+			console.log(formData);
+      
+      // Submit the form using AJAX.
+      $.ajax({
+          type: 'POST',
+          url: $(form).attr('action'),
+          data: formData
+      })
+      
+    	//successful response
+      .done(function(response) {
+    		// Make sure that the formMessages div has the 'success' class.
+    		$(formMessages).removeClass('error');
+    		$(formMessages).addClass('success');
+
+        // Set the message text.
+        $(formMessages).text(response);
+
+        // Clear the form.
+        $('#first-name').val('');
+        $('#last-name').val('');
+        $('#email').val('');
+        $('#phone').val('');
+        $('#message').val('');
+    	})
+
+			//failed response
+      .fail(function(data) {
+        // Make sure that the formMessages div has the 'error' class.
+        $(formMessages).removeClass('success');
+        $(formMessages).addClass('error');
+				
+        console.log("dataresponse: "+ data.responseText);
+        // Set the message text.
+        if (data.responseText !== '') {
+            $(formMessages).text(data.responseText);
+        } else {
+            $(formMessages).text('Oops! An error occured and your message could not be sent.');
+        }
+    });
+      
+      
+      // TODO
+  });
+
+});
+
+
+
+
+
 function hasTouch() {
     return 'ontouchstart' in document.documentElement
            || navigator.maxTouchPoints > 0
